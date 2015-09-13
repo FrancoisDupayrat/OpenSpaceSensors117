d065284b7b8b096899f0e452b73a1e96bed9e1b8
François Dupayrat <fradow@gmail.com>
2015-09-13 10:58:13 +0200

    Modify main for web server

--- main.py
+++ main.py
@@ -7,32 +7,55 @@ import sound
 import lcd
 import air_quality
 import time
+import ConfigParser
 
-#Temperature use Analog pin 1
-temperature.init()
 
-#LED use Digital pin 13
-led.init()
+def getValues():
+    config = ConfigParser.ConfigParser()
+    config.read("values.ini")
+    db = config.get("Values", "db")
+    degree = config.get("Values", "degree")
+    lux = config.get("Values", "lux")
+    quality = config.get("Values", "quality")
+    data = [db, degree, lux, quality]
+    return data
 
-#Light is on I2C, doesn't use a specific pin
-light.init()
+def init():
+    #Temperature use Analog pin 1
+    temperature.init()
 
-#Sound uses A0 Grove Port
-sound.init()
+    #LED use Digital pin 13
+    led.init()
 
-#LCD is on I2C, doesn't use a specific pin
-lcd.init()
-lcd.setRGB(0,255,0)
+    #Light is on I2C, doesn't use a specific pin
+    light.init()
 
-#Air Quality is on Analog Pin 1
-air_quality.init()
+    #Sound uses A0 Grove Port
+    sound.init()
 
-while True:  
-	led.loop() #will add a 0.4sec delay
-	db = sound.loop() # time.sleep 0.5s
-	degree = temperature.loop()
-	lux = light.loop()
-	quality = air_quality.loop()
-	lcd.setText("%.0f lux, %.01f C\n%d db, %s" % (lux, degree, db, quality))
-	time.sleep(.5)
+    #LCD is on I2C, doesn't use a specific pin
+    lcd.init()
+    lcd.setRGB(0,255,0)
 
+    #Air Quality is on Analog Pin 1
+    air_quality.init()
+
+if __name__=="__main__":
+    init()
+    config = ConfigParser.ConfigParser()
+    config.add_section('Values')
+    while True:  
+    	led.loop() #will add a 0.4sec delay
+    	db = sound.loop() # time.sleep 0.5s
+    	degree = temperature.loop()
+    	lux = light.loop()
+    	quality = air_quality.loop()
+    	lcd.setText("%.0f lux, %.01f C\n%d db, %s" % (lux, degree, db, quality))
+        cfgfile = open("values.ini",'w')
+        config.set('Values', 'db', db)
+        config.set('Values', 'lux', lux)
+        config.set('Values', 'degree', degree)
+        config.set('Values', 'quality', quality)
+        config.write(cfgfile)  
+        cfgfile.close()
+    	time.sleep(.5)
