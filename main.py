06013d6e7b8c9c882141cfde871773130448334e
François Dupayrat <fradow@gmail.com>
2015-09-13 12:59:28 +0200

    Fix min/max error and missing value in getValues

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def getValues():
     lux = config.get("Values", "lux")
     quality = config.get("Values", "quality")
     qualityValue = config.get("Values", "qualityValue")
-    data = [db, degree, lux, qualityValue]
+    data = [db, degree, lux, quality, qualityValue]
     return data
 
 def init():
@@ -76,11 +76,11 @@ if __name__=="__main__":
         led_bar.setLevel(level)
         
         cfgfile = open("values.ini",'w')
-        config.set('Values', 'db', max(db / 5, 100)) # in percent
+        config.set('Values', 'db', min(db / 5, 100)) # in percent
         config.set('Values', 'lux', round(lux,0)) # in lux
         config.set('Values', 'degree', round(degree,1)) # in degree
         config.set('Values', 'quality', quality) # string
-        config.set('Values', 'qualityValue', max(qualityValue/10, 100)) #in percent
+        config.set('Values', 'qualityValue', min(qualityValue/10, 100)) #in percent
         config.write(cfgfile)  
         cfgfile.close()
     	time.sleep(.5)
